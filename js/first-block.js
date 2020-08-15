4ac40e4d644b14f60009ae62dd7e5531a489f56b
Maksim Babain <mababain@mail.ru>
2020-08-15 15:25:15 +0700

    added first-block anim

--- js/first-block.js
+++ js/first-block.js
@@ -1,5 +1,7 @@
 const bubble = document.querySelectorAll('.bubble');
-const firstBlock = document.getElementById('scene')
+const firstBlock = document.getElementById('scene');
+
+const animItems = document.querySelectorAll('.anim-items');
 
 document.addEventListener('mousemove' , parralax);
 
@@ -12,4 +14,15 @@ function parralax(e){
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
     });
+}
+
+
+if (animItems.length > 0) {
+    anim()
+    function anim(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            animItem.classList.add("active");
+        }
+    }
 }
