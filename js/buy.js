c083dc86cfb0e2c94ca748d553dcfb672464802e
Maksim Babain <mababain@mail.ru>
2020-08-16 19:01:50 +0700

    Fixed modal window

--- js/buy.js
+++ js/buy.js
@@ -1,11 +1,14 @@
 const buyButton = document.querySelector('.section-product-descripton-info__button');
 const modal = document.querySelector('.modal');
 const formCloseButton = document.querySelector('.form__button-close');
+const modalBackground = document.querySelector('.modal-background');
 
 buyButton.addEventListener('click', () => {
     modal.classList.toggle('active--modal');
+    modalBackground.classList.toggle('active-modal--bg');
 });
 
 formCloseButton.addEventListener('click', () => {
     modal.classList.remove('active--modal');
+    modalBackground.classList.remove('active-modal--bg');
 });
